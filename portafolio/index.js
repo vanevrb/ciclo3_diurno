f23fee653f0202ca2e6389589dec1b7bcbf7a9fd
Vanessa Rodriguez <vane.vrb@gmail.com>
2021-10-15 08:10:40 -0500

    Add files via upload

---
+++ portafolio/index.js
@@ -0,0 +1,17 @@
+function cambiarcolor(nuevocolor){
+    var about = document.getElementById("about");
+    about.style.color = nuevocolor;
+}
+
+// var contact = document.getElementById("contact");
+
+
+
+// var contenido1 = document.getElementById("contenido1");
+
+
+function cambiartamano(nuevotamano){
+    var imagen = document.getElementById("imagen");
+    imagen.style.width=nuevotamano;
+}
+    
